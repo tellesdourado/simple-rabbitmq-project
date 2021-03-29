60ea31549973fec15cb799fb328ab18899d8b9ad
Teles Dourado <tellesdourado@gmail.com>
2021-03-29 14:41:33 -0300

    adicionando test para validação de parametro invalido

--- __tests__/message.test.ts
+++ __tests__/message.test.ts
@@ -1,20 +1,28 @@
 import { app } from "../src/app";
 import request from "supertest";
 
-describe("GET /message - endpoint de busca mensagens", () => {
-  it("deve retornar status 200 e status e as mensagens da fila", async () => {
+describe("GET /message - endpoint to find messages", () => {
+  it("should return code status 200 and get messages in queue", async () => {
     const result = await request(app).get("/message");
     expect(result.status).toEqual(200);
     expect(JSON.stringify(result.header)).toMatch(/application\/json/);
   });
 });
 
-describe("POST /message - endpoint de adição de mensagens", () => {
-  it("deve adicionar uma mensagem na fila", async () => {
+describe("POST /message - endpoint de to add new message", () => {
+  it("should add new message in queue", async () => {
     const result = await request(app)
       .post("/message")
-      .send({ message: "é um test" });
+      .send({ message: "is just a test" });
     expect(result.status).toEqual(200);
+    expect(JSON.stringify(result.header)).toMatch(/application\/json/);
+  });
+
+  it("should fail when invalid paramenters are passed", async () => {
+    const result = await request(app)
+      .post("/message")
+      .send({ invalid: "is just a test" });
+    expect(result.status).toEqual(400);
     expect(JSON.stringify(result.header)).toMatch(/application\/json/);
   });
 });
