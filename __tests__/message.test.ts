142a6d608c2a15abdbc82b8bde6a5400091b30fd
Teles Dourado <tellesdourado@gmail.com>
2021-03-29 09:49:55 -0300

    adicionando teste para enpoint de envio de mensagem

--- __tests__/message.test.ts
+++ __tests__/message.test.ts
@@ -1,9 +1,19 @@
 import { app } from "../src/app";
 import request from "supertest";
 
-describe("GET /message - endpoint de mensagens", () => {
+describe("GET /message - endpoint de busca mensagens", () => {
   it("deve retornar status 200 e status e as mensagens da fila", async () => {
     const result = await request(app).get("/message");
+    expect(result.status).toEqual(200);
+    expect(JSON.stringify(result.header)).toMatch(/application\/json/);
+  });
+});
+
+describe("POST /message - endpoint de adição de mensagens", () => {
+  it("deve adicionar uma mensagem na fila", async () => {
+    const result = await request(app)
+      .post("/message")
+      .send({ message: "é um test" });
     expect(result.status).toEqual(200);
     expect(JSON.stringify(result.header)).toMatch(/application\/json/);
   });
